cbf0e6be9488672507de4e5a87f79755fefe78af
dahliarosni <154412982+dahliarosni@users.noreply.github.com>
2023-12-21 14:52:40 +0800

    Add files via upload

---
+++ sales-generate-predict.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+
+st.write("# Simple Sales Advertidsing Prediction App")
+st.write("This app predicts the **Sales** type!")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.7, 296.4, 147.04)
+    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 23.26)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 30.55)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+
+st.subheader('Prediction')
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+
+loaded_modellinear = pickle.load(open("Salespred.h5", "rb"))
+TV=loaded_modellinear.predict(df)
+st.write(prediction_proba)
